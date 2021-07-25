60d3156eea89ef130cb78c0f972b96610a218bef
Abner50a <soporteabner@gmail.com>
2021-07-25 07:07:40 -0500

    Creando interface

---
+++ 1-app/index.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
+
+//Creamos interface para definir la estrucutra del objeto
+interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+}
+
+axios.get(url).then(response => {
+
+  /**
+ * 
+ * Como buscar errores con tyscript usando interface
+ * 
+ * 
+ */
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const userid = todo.userId;
+  const title = todo.title;
+
+
+
+
+  console.log(`Todo  id : ${id} 
+    titulo : ${title}
+    usuario : ${userid}
+  `);
+})
