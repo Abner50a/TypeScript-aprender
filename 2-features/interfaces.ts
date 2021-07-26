3d83b325327943349e7a48296422593a17e81bc1
Abner50a <soporteabner@gmail.com>
2021-07-26 15:38:26 -0500

    Interface parte 2

--- 2-features/interfaces.ts
+++ 2-features/interfaces.ts
@@ -1,4 +1,4 @@
-//Interfacesd
+/* //Interfacesd
 
 interface Carro {
   nombre : string,
@@ -16,9 +16,51 @@ const carrosViejos = {
   }
 };
 
+const drink = {
+  color: 'cafe',
+  carbonada: true,
+  azucar: 40,
+  summary() : string {
+    return `Bebida tiene: ${this.color} ${this.azucar}`
+  }
+}
+
 const imprimirCarro = (carro:  Carro): void => {
   //console.log(`Nombre :${carro.nombre} Año : ${carro.anio}, Rompe : ${carro.seRompio}`)
   console.log(carro.summary())
 };
 
 imprimirCarro(carrosViejos);
+ */
+
+//Interfacesd parte 2
+
+interface Reporte {
+  summary() : string;
+}
+
+const carrosViejos = {
+  nombre: 'tsuru',
+  anio: new Date(),
+  seRompio: true,
+  summary() : string {
+    return `Name: ${this.nombre}`
+  }
+};
+
+const drink = {
+  color: 'cafe',
+  carbonada: true,
+  azucar: 40,
+  summary() : string {
+    return `Bebida tiene: ${this.color} ${this.azucar}`
+  }
+}
+
+const imprimirSummary = (item:  Reporte): void => {
+  //console.log(`Nombre :${carro.nombre} Año : ${carro.anio}, Rompe : ${carro.seRompio}`)
+  console.log(item.summary())
+};
+
+imprimirSummary(carrosViejos);
+imprimirSummary(carrosViejos);
